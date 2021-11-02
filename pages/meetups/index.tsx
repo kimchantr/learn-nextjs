3e9f313c5fd1c085a0361e82fe6774280c308a6b
Kim Chan <chantran.24hdev@gmail.com>
2021-11-02 18:29:33 +0700

    Meetup list page with getStaticProps

--- pages/meetups/index.tsx
+++ pages/meetups/index.tsx
@@ -1,9 +1,38 @@
-function MeetupListPage() {
+import { GetStaticProps, GetStaticPropsContext } from 'next'
+import Head from 'next/head'
+import { MeetupList } from '../../components/meetup'
+import { Meetup } from '../../models'
+
+export interface MeetupListPageProps {
+  meetups: Meetup[]
+}
+
+function MeetupListPage({ meetups }: MeetupListPageProps) {
+  console.log(meetups)
+
   return (
-    <div>
-      <h1>Meetup list page</h1>
-    </div>
+    <>
+      <Head>
+        <title>Meetups</title>
+        <meta name="description" content="Browse a huge list of highly active meetups!" />
+      </Head>
+
+      <MeetupList meetups={meetups} />
+    </>
   )
+}
+
+export const getStaticProps: GetStaticProps<MeetupListPageProps> = async (
+  context: GetStaticPropsContext
+) => {
+  const res = await fetch('https://json-server-kctrnn.herokuapp.com/api/meetups')
+  const meetups = await res.json()
+
+  return {
+    props: {
+      meetups,
+    },
+  }
 }
 
 export default MeetupListPage
