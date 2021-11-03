519aab6f0d800ce0a3833931dd499228bd0c6a14
Kim Chan <chantran.24hdev@gmail.com>
2021-11-03 12:46:17 +0700

    Add create meetup page with API route

--- pages/meetups/create.tsx
+++ pages/meetups/create.tsx
@@ -1,8 +1,38 @@
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import AddMeetupForm from '../../components/meetup/AddMeetupForm'
+import { Meetup } from '../../models'
+
 function MeetupCreatePage() {
+  const router = useRouter()
+
+  const handleAddMeetupFormSubmit = async (formValues: Meetup) => {
+    const response = await fetch('/api/meetups/create', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formValues),
+    })
+
+    const responseJSON = await response.json()
+    console.log(responseJSON)
+
+    router.push('/meetups')
+  }
+
   return (
-    <div>
-      <h1>Create meetup page</h1>
-    </div>
+    <>
+      <Head>
+        <title>Add a New Meetup</title>
+        <meta
+          name="description"
+          content="Add your own meetups and create amazing networking opportunities"
+        />
+      </Head>
+
+      <AddMeetupForm onSubmit={handleAddMeetupFormSubmit} />
+    </>
   )
 }
 
